14b66968420bb437d5c52d98391e9f5b1ccb4a91
ZumaLux <zumalux99@gmail.com>
2023-05-14 06:29:06 +0300

    nav structure done

--- src/layouts/navbar/Navbar.jsx
+++ src/layouts/navbar/Navbar.jsx
@@ -1,39 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
-// import logo from "../../assets/rental-heaven-logo-transparent-low-res.png";
+import logo from "../../assets/rental-heaven-logo.png";
 import { FaBars as BurgirIcon } from "react-icons/fa";
-// import { CgClose as BurgirClose } from "react-icons/cg";
+import { CgClose as BurgirClose } from "react-icons/cg";
 
 const Navbar = () => {
+  const [navActive, setNavActive] = useState(false);
   return (
     <header>
       <nav>
         <div className="nav-logo">
           <Link to="/">
-            <img src={""} alt="" />
-            LOGO
+            <img src={logo} alt="" />
           </Link>
         </div>
-        <div className="nav-links">
-          <ul>
+        <div className="nav-right">
+          <div className={`nav-links ${!navActive && "nav-hidden"}`}>
             <Link to="/">Home</Link>
             <Link to="/cars">Cars</Link>
             <Link to="/contacts">Contact</Link>
-
             <>
               <Link to="/customers">Customers</Link>
               <Link to="/rentals">Rentals</Link>
             </>
-
             <Link to="/signIn">Sign In</Link>
-
-            {/* <CustomLink showSubNav={""} onMouseEnter={() => ""} onMouseLeave={() => ""}></CustomLink> */}
-
-            {/* <CustomLink to="/signUp">SignUp</CustomLink> */}
-          </ul>
-          <div className="burgir-menu" onClick={() => ""}>
-            {<BurgirIcon />}
+          </div>
+          <div className="burgir-menu" onClick={() => setNavActive((prev) => !prev)}>
+            {navActive ? <BurgirClose /> : <BurgirIcon />}
           </div>
         </div>
       </nav>
