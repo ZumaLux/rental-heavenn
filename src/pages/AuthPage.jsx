9ddfba15aaf3091ab318fec3caad1897c531fa6a
ZumaLux <zumalux99@gmail.com>
2023-05-20 00:16:31 +0300

    makes sure that after you switch back to SignIn page you'll see the login first and not the register

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./AuthPage.css";
 import LoginForm from "../components/modals/LoginForm";
 import RegisterForm from "../components/modals/RegisterForm";
+import { useFormContext } from "../context/formContext";
 
 const AuthPage = () => {
+  const { closeRegister } = useFormContext();
+
+  useEffect(() => {
+    // resets back to login form next time you visit SignUp page
+    closeRegister();
+  }, []);
+
   return (
     <div className="page-container">
       <div className="auth-page">
