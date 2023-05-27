70b3fb12bcf6d816d92427b8738ee18ece14e2fc
ZumaLux <zumalux99@gmail.com>
2023-05-27 22:01:26 +0300

    Single car page styling

--- src/pages/SingleCar.jsx
+++ src/pages/SingleCar.jsx
@@ -27,7 +27,7 @@ const SingleCar = () => {
 
   useEffect(() => {
     const shrinkHeaderOnScroll = () => {
-      if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
         headerRef.current.className = "single-car__header header-shrink";
       } else {
         headerRef.current.className = "single-car__header";
@@ -53,44 +53,86 @@ const SingleCar = () => {
   return (
     <div className="page-container">
       <div ref={headerRef} className="single-car__header">
-        <h1>
+        <h1 className="single-car__header-title">
           {data.brand} {data.model}
         </h1>
-        <div className="rent-btn">
-          <Button label="Rent now" />
+        <div className="rent-btn-container">
+          <div className="rent-price">$ {data.price}</div>
+          <div className="rent-btn">
+            <Button label="Rent now" />
+          </div>
         </div>
       </div>
-      <div className="single-car__basic-info">
-        <div className="img-container">
-          <img src={data.image} alt="image" />
-        </div>
-        <div className="info-container">
-          <div className="info">
-            <p>
-              <CalenadrIcon />
-              &nbsp; {data.year}
-            </p>
-            <p>
-              <FuelIcon />
-              &nbsp; {data.fuel}
-            </p>
-            <p>
-              <SeatIcon />
-              &nbsp; {data.seats}
-            </p>
-            <p>
-              <GearboxIcon />
-              &nbsp; {data.gearbox}
-            </p>
-            <p>
-              <DoorIcon />
-              &nbsp; {data.doors}
-            </p>
-            <p>
-              <AcIcon />
-              &nbsp; {data.ac}
-            </p>
+
+      <div className="single-car__basic-info-container">
+        <h1 className="single-car__basic-info-heading">
+          {data.brand} {data.model}
+        </h1>
+        <div className="single-car__basic-info">
+          <div className="img-container">
+            <img src={data.image} alt="image" />
           </div>
+          <div className="info-container">
+            <div className="info">
+              <p>
+                <CalenadrIcon />
+                &nbsp; {data.year}
+              </p>
+              <p>
+                <FuelIcon />
+                &nbsp; {data.fuel}
+              </p>
+              <p>
+                <SeatIcon />
+                &nbsp; {data.seats}
+              </p>
+              <p>
+                <GearboxIcon />
+                &nbsp; {data.gearbox}
+              </p>
+              <p>
+                <DoorIcon />
+                &nbsp; {data.doors}
+              </p>
+              <p>
+                <AcIcon />
+                &nbsp; {data.ac}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="single-car__details-container">
+        <div className="single-car__details">
+          <h1 className="single-car__details-heading">Details</h1>
+          {Object.keys(data).map((key, index) => {
+            if (
+              key === "id" ||
+              key === "image" ||
+              key === "discount" ||
+              key === "price" ||
+              key === "discountPrice"
+            )
+              return "";
+
+            return (
+              <div className="row-container" key={index}>
+                <div className="row-title">
+                  <b>{key.toUpperCase()}</b>
+                </div>
+                <div className="row-info">{data[key]}</div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="single-car__description">
+          <h1 className="single-car__details-heading">Description</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam libero possimus totam,
+            aliquam neque unde iste, necessitatibus tenetur alias pariatur tempore numquam omnis
+            quam veniam sapiente quis iusto, nesciunt quo?
+          </p>
         </div>
       </div>
 
@@ -116,3 +158,25 @@ const SingleCar = () => {
 };
 
 export default SingleCar;
+
+// {
+//   Object.keys(car).map((key, index) => {
+//     if (
+//       key === "id" ||
+//       key === "img" ||
+//       key === "discount" ||
+//       key === "price" ||
+//       key === "discPrice"
+//     )
+//       return "";
+
+//     return (
+//       <div className="row" key={index}>
+//         <div className="row-title">
+//           <b>{key.toUpperCase()}</b>
+//         </div>
+//         <div className="row-info">{car[key]}</div>
+//       </div>
+//     );
+//   });
+// }
